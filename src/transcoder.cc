79fc32e31fe946c2cb541663fba682580f5f39ef
icetd <tiandengzbc@gmail.com>
2026-06-15 12:04:14 +0800

    fixed video buffer dymamic

--- src/transcoder.cc
+++ src/transcoder.cc
@@ -71,6 +71,7 @@ void TransCoder::init()
 
 void TransCoder::run()
 {
+    uint8_t *buffer;
     timeval tv;
     for (;;) {
         tv.tv_sec = 1;
@@ -78,8 +79,8 @@ void TransCoder::run()
         int startCode = 0;
         int ret = capture->isReadable(&tv);
         if (ret == 1) {
-            uint8_t buffer[capture->getBufferSize()];
-            int resize = capture->read((char *)buffer, sizeof(buffer));
+            buffer = (uint8_t *)malloc(capture->getBufferSize());
+            int resize = capture->read((char *)buffer, capture->getBufferSize());
             frameSize = 0;
             if (config.format == "MJPEG") {
                 ret = decompress->tjpeg2yuv(buffer, resize, &yuv_buf, &yuv_size);
@@ -99,6 +100,7 @@ void TransCoder::run()
 
             if (onEncodedDataCallback && frameSize > 0) {
                 onEncodedDataCallback(std::vector<uint8_t>(encodeData, encodeData + frameSize));
+                free(buffer);
             }
 
         } else if (ret == -1) {
